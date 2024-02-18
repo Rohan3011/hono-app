623cd9f68835dc3e9e1fd7c77d13e82a3766a52b
rohanop <rohankamble3011@gmail.com>
2024-02-18 21:27:13 +0530

    remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
 import { logger } from 'hono/logger';
-import { db } from './db';
-import { todos } from './db/schemas/todo';
-import { eq } from 'drizzle-orm';
 import todoRouter from './routes/todo';
 const app = new Hono();
 
@@ -20,5 +17,3 @@ app.notFound((c) => {
 serve(app, (info) => {
 	console.log(`Listening on http://localhost:${info.port}`);
 });
-
-// export default app;
